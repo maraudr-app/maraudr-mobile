f6c4727a8aeb24c51858783c889083c604753eaf
xororist <hcachon@myges.fr>
2025-07-09 21:11:17 +0200

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
-  static const String baseUrlAuth = 'https://app.maraudr.eu/user';
-  static const String baseUrlAssociation = 'https://app.maraudr.eu/association';
-  static const String baseUrlStock = 'https://app.maraudr.eu/stock';
-  static const String baseUrlGeo = 'https://app.maraudr.eu/geo';
+  static const String baseUrlAuth = 'https://api.maraudr.eu/user';
+  static const String baseUrlAssociation = 'https://api.maraudr.eu/association';
+  static const String baseUrlStock = 'https://api.maraudr.eu/stock';
+  static const String baseUrlGeo = 'https://api.maraudr.eu/geo';
 }

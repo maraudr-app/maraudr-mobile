595bb130f9bcad68044a6172972b44620ac58c13
xororist <hcachon@myges.fr>
2025-07-09 19:18:57 +0200

    changing conf file

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
-  static const String baseUrlAuth = 'http://192.168.1.53:8082';
-  static const String baseUrlAssociation = 'http://192.168.1.53:8080';
-  static const String baseUrlStock = 'http://192.168.1.53:8081';
-  static const String baseUrlGeo = 'http://192.168.1.53:8084';
+  static const String baseUrlAuth = 'https://app.maraudr.eu/user';
+  static const String baseUrlAssociation = 'https://app.maraudr.eu/association';
+  static const String baseUrlStock = 'https://app.maraudr.eu/stock';
+  static const String baseUrlGeo = 'https://app.maraudr.eu/geo';
 }
